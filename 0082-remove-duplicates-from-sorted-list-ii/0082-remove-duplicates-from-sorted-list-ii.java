77e5e397adf1db12a646dec22bfc3db8f9c9bf55
Prajwal Kundar <prajwalkundar8746@gmail.com>
2026-09-25 19:07:19 +0530

    Time: 0 ms (100%), Space: 44.9 MB (72.18%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,26 +10,44 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        ListNode temp=head;
-        int previous;
-        while(temp!=null){
-            map.put(temp.val,map.getOrDefault(temp.val,0)+1);
-            temp=temp.next;
-        }
-        TreeSet<Integer> set=new TreeSet<>();
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()==1){
-                set.add(entry.getKey());
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // ListNode temp=head;
+        // int previous;
+        // while(temp!=null){
+        //     map.put(temp.val,map.getOrDefault(temp.val,0)+1);
+        //     temp=temp.next;
+        // }
+        // TreeSet<Integer> set=new TreeSet<>();
+        // for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+        //     if(entry.getValue()==1){
+        //         set.add(entry.getKey());
+        //     }
+        // }
+        // ListNode res=new ListNode(0);
+        // ListNode answer=res;
+        // for(int values:set){
+        //     ListNode newnode=new ListNode(values);
+        //     res.next=newnode;
+        //     res=res.next;
+        // }
+        // return answer.next;
+       ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) {
+                while (curr.next != null &&
+                       curr.val == curr.next.val) {
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            } 
+            else {
+                prev = prev.next;
             }
+            curr = curr.next;
         }
-        ListNode res=new ListNode(0);
-        ListNode answer=res;
-        for(int values:set){
-            ListNode newnode=new ListNode(values);
-            res.next=newnode;
-            res=res.next;
-        }
-        return answer.next;
+        return dummy.next;
     }
 }
